dc11c90602cd3a73e21104a05ccbe5fe5f342259
Fabian Torres <fabi.ignacio1998@gmail.com>
2023-02-16 19:24:24 -0300

    Componentes Login, Logout, modificacion index.js para el uso de auth0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +9,17 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-cz6i21an2opri7kv.us.auth0.com"
+      clientId="Op3PZMpZSk3hNvSTG9qRurhQip9WssPh"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "this is a unique identifier viator final",
+        scope: "  openid profile email  ",
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
