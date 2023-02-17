4d53535f02f6b0d7cf1f12826c191292732f22a8
Code-Zeal <codeseazeal@gmail.com>
2023-02-17 10:32:49 -0400

    Resolviendo errores que se generaron por conflictos

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import FlightSearch from "./FlightSearch";
 import Footer from "./Footer";
+import Recommendations from "./Recommendations.jsx";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-gray-200">
       <NavBar />
       <FlightSearch />
+      <Recommendations />
       <Footer />
     </div>
   );
