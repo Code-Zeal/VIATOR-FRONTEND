49be79da7f9af880ace770a366a51258aecf2d37
Volkyr <yatel.lara@gmail.com>
2023-02-20 13:53:12 -0300

    Añadida la ruta Landing, instaladas dependecias postcss y autoprefixer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./Components/Landing";
 import Home from "./Components/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/" component={Landing} />
         <Route path="/home" component={Home} />
       </Switch>
     </Router>
