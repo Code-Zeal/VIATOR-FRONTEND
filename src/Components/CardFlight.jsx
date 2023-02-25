581d2b9e804413f74f7a5c6a6d1e88161825de69
Code-Zeal <codeseazeal@gmail.com>
2023-02-25 12:02:10 -0400

    cambio de comprar a detalles y svg de estrella

--- src/Components/CardFlight.jsx
+++ src/Components/CardFlight.jsx
@@ -55,56 +55,36 @@ const CardFlight = (props) => {
                 <>
                   <svg
                     className="cursor-pointer my-4 lg:mx-4 lg:my-0"
-                    onClick={() => setIsFav(false)}
                     xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => setIsFav(false)}
                     width="50px"
                     height="50px"
-                    viewBox="0 -1 20 20"
+                    viewBox="0 0 24 24"
+                    fill="#E9134C"
                   >
-                    <g id="star" transform="translate(-2 -3)">
-                      <path
-                        id="secondary"
-                        fill="#2ca9bc"
-                        d="M12,4,9.22,9.27,3,10.11l4.5,4.1L6.44,20,12,17.27,17.56,20,16.5,14.21l4.5-4.1-6.22-.84Z"
-                      />
-                      <path
-                        id="primary"
-                        d="M12,4,9.22,9.27,3,10.11l4.5,4.1L6.44,20,12,17.27,17.56,20,16.5,14.21l4.5-4.1-6.22-.84Z"
-                        fill="none"
-                        stroke="#000000"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                      />
-                    </g>
+                    <path
+                      d="M4.42602 12.9469L10.1622 19.1217C11.1546 20.1899 12.8454 20.1899 13.8378 19.1217L19.574 12.9469C21.4753 10.9002 21.4753 7.58179 19.574 5.53505C17.6726 3.48832 14.5899 3.48832 12.6885 5.53505V5.53505C12.3168 5.93527 11.6832 5.93527 11.3115 5.53505V5.53505C9.4101 3.48832 6.32738 3.48832 4.42602 5.53505C2.52466 7.58178 2.52466 10.9002 4.42602 12.9469Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                    />
                   </svg>
                 </>
               ) : (
                 <>
                   <svg
                     className="cursor-pointer my-4 lg:mx-4 lg:my-0"
-                    onClick={() => setIsFav(true)}
                     xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => setIsFav(true)}
                     width="50px"
                     height="50px"
-                    viewBox="0 -1 20 20"
+                    viewBox="0 0 24 24"
+                    fill="none"
                   >
-                    <g id="star" transform="translate(-2 -3)">
-                      <path
-                        id="secondary"
-                        fill="none"
-                        d="M12,4,9.22,9.27,3,10.11l4.5,4.1L6.44,20,12,17.27,17.56,20,16.5,14.21l4.5-4.1-6.22-.84Z"
-                      />
-                      <path
-                        id="primary"
-                        d="M12,4,9.22,9.27,3,10.11l4.5,4.1L6.44,20,12,17.27,17.56,20,16.5,14.21l4.5-4.1-6.22-.84Z"
-                        fill="none"
-                        stroke="#000000"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                      />
-                    </g>
+                    <path
+                      d="M4.42602 12.9469L10.1622 19.1217C11.1546 20.1899 12.8454 20.1899 13.8378 19.1217L19.574 12.9469C21.4753 10.9002 21.4753 7.58179 19.574 5.53505C17.6726 3.48832 14.5899 3.48832 12.6885 5.53505V5.53505C12.3168 5.93527 11.6832 5.93527 11.3115 5.53505V5.53505C9.4101 3.48832 6.32738 3.48832 4.42602 5.53505C2.52466 7.58178 2.52466 10.9002 4.42602 12.9469Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                    />
                   </svg>
                 </>
               )}
@@ -191,7 +171,7 @@ const CardFlight = (props) => {
           </div>
 
           <div className="flex items-center justify-center bg-[#E9134C] text-[white]   w-full py-4  lg:w-1/5 rounded-tl-2xl rounded-tr-2xl lg:rounded-tr-none cursor-pointer text-2xl lg:rounded-tl-2xl lg:rounded-bl-2xl font-bold lg:py-12">
-            Comprar
+            Detalles
           </div>
         </div>
       </div>
