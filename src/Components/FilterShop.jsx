3904e74ec5d67455cc7e040363be79ca2c3fbbae
Code-Zeal <codeseazeal@gmail.com>
2023-02-27 09:01:47 -0400

    estilo en los filtros

--- src/Components/FilterShop.jsx
+++ src/Components/FilterShop.jsx
@@ -40,24 +40,49 @@ const FilterShop = () => {
   console.log(getAirports);
 
   return (
-    <div className="">
-      <div>
-        <h1>filtros aeropuertos</h1>
-        {getAirports.map((air) => (
-          <button className="p-4" onClick={filtrosAirportFn} value={air.name}>
-            {air.name}
-          </button>
-        ))}
+    <div className=" flex items-center bg-azulOscuro text-[white] py-2 w-screen px-4 ">
+      <img
+        className="hidden md:flex animate-pulse px-2"
+        src="https://i.imgur.com/clNpPpW.png"
+        alt=""
+      />
+      <div className="flex flex-col w-full items-center">
+        <h1 className="text-lg md:text-3xl font-bold bg-[white] text-[black] px-10 py-1 rounded-tr-2xl rounded-bl-2xl">
+          Filtros
+        </h1>
+        <h2 className="font-bold lg:text-xl py-2">Aeropuertos</h2>
+        <div className="flex">
+          {getAirports.map((air) => (
+            <button
+              className="px-2 py-1 bg-blanco text-[black]  rounded-lg hover:bg-[#6644c1] hover:text-[white] hover:shadow-lg hover:shadow-[white] md:m-2 m-1"
+              onClick={filtrosAirportFn}
+              value={air.name}
+            >
+              {air.name}
+            </button>
+          ))}
+        </div>
+        <div className="">
+          <select
+            onChange={handleChangeScale}
+            name=""
+            id=""
+            className="text-[black] my-2 rounded-md"
+          >
+            <option value="default" selected="true" disabled="true">
+              Escalas
+            </option>
+            <option value="0">Sin Escalas</option>
+            <option value="1">1 Escala</option>
+            <option value="2">2 Escalas</option>
+          </select>
+        </div>
       </div>
-      <div>
-        <h1>Filtros por escala</h1>
-        <select onChange={handleChangeScale} name="" id="">
-          <option value="default">escalas</option>
-          <option value="0">Sin escalas</option>
-          <option value="1">con 1 escalas</option>
-          <option value="2">con 2 escalas</option>
-        </select>
-      </div>
+      <img
+        className=" hidden animate-pulse px-2 md:flex"
+        src="https://i.imgur.com/clNpPpW.png"
+        alt=""
+      />
     </div>
   );
 };
