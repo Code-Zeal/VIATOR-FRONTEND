f377936695e77040a68d4a1367fac7dbb94a528d
Volkyr <yatel.lara@gmail.com>
2023-03-01 21:53:06 -0300

    Componente LoadingPage añadido y sumado al Shop

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -5,17 +5,34 @@ import { useSelector } from "react-redux";
 import CardsFlight from "./CardsFlight";
 import FlightSearch from "./FlightSearch";
 import FilterShop from "./FilterShop";
+import { useState, useEffect } from "react";
+import LoadingPage from "./LoadingPage";
 
 export default function Shop() {
+  const [isLoading, setIsLoading] = useState(true);
   const flights = useSelector((state) => state.searchedFlights);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // espera 3 segundos antes de mostrar la aplicación
+  }, []);
+
   return (
-    <div className="h-screen w-screen bg-gray-200">
-      <NavBar />
-      <FilterShop />
-      <FlightSearch />
-      <CardsFlight />
-      <Footer />
+    <div className={isLoading ? "loading-page" : ""}>
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <div>
+          <div className="h-screen w-screen bg-gray-200">
+            <NavBar />
+            <FilterShop />
+            <FlightSearch />
+            <CardsFlight />
+            <Footer />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
